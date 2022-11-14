20854bceea1fe1fc377fbbdf39be288126f156ec
JJWRoeloffs <jelleroeloffs@gmail.com>
2022-11-14 17:56:20 +0100

    bump version

---
+++ src/dynamicfluency/__init__.py
@@ -0,0 +1,11 @@
+"""
+The base python package for DynamicFluency:
+Monitor and understand the dynamicity of linguistic aspects in (L2) speech.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:license: AGPL-3.0, see LICENSE for more details.
+"""
+
+__title__ = "DynamicFluency"
+__author__ = "JJWRoeloffs"
+__license__ = "AGPL-3.0"
+__version__ = "0.0.2"
